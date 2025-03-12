e73a2fe4ac8d2c3085c6c4e18f2b117151047561
Maël Abigail <mael@urside.net>
2025-03-12 15:31:02 +0100

    Update PlayerEditorManager.java

--- src/main/java/io/github/rypofalem/armorstandeditor/PlayerEditorManager.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/PlayerEditorManager.java
@@ -424,7 +424,7 @@ public class PlayerEditorManager implements Listener {
             if (item != null && item.hasItemMeta()) {
                 Player player = (Player) e.getWhoClicked();
                 String command = item.getItemMeta().getPersistentDataContainer().get(plugin.getIconKey(), PersistentDataType.STRING);
-                if (command != null && command.equals("")) {
+                if (command != null && command.equals("ase ")) {
                     return;
                 } else if (command != null) {
                     player.performCommand(command);
